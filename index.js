c771d5c7dea5b4fe080ccd62caf0b58ff7179602
discordissus <125839231+discordissus@users.noreply.github.com>
2024-10-07 02:20:33 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const Display = document.getElementById("text");
+
+function GotoDisplay(input){
+    Display.value += input;
+}
+
+function Clear(){
+    Display.value = "";
+}
+
+function Calculate(){
+    try{
+        Display.value = eval(Display.value);
+    }
+    catch{
+        Display.value = "😟wrong";
+        setTimeout(Clear, 1500)
+    }
+
+
+    Display.value = eval(Display.value);
+}
